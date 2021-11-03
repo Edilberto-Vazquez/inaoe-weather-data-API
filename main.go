e2ea509dff77ff78a05027cea8569e1d7494a0f2
Edilberto Vazquez Luna <edi_vazquez12@hotmail.com>
2021-11-03 16:24:09 -0600

    delete comments on main file

--- main.go
+++ main.go
@@ -1,28 +1,17 @@
 package main
 
 import (
-	"github.com/Edilberto-Vazquez/inaoe-weather-data-API/db/migrations"
 	"github.com/Edilberto-Vazquez/inaoe-weather-data-API/libs"
-	"github.com/Edilberto-Vazquez/inaoe-weather-data-API/services"
 )
 
 func main() {
-	// routes.Run()
-	// efm := etl.ProcesNewFile("/mnt/f/DataSets/Conjuntos-originales/medidor-campo-electrico/INAOE parque-01102019.efm", "efm")
-	// log := etl.ProcesNewFile("/mnt/f/DataSets/Conjuntos-originales/medidor-campo-electrico/EFMEvents.log", "log")
-	// wc := etl.ProcesNewFile("/mnt/f/DataSets/Conjuntos-originales/estacion-meteorologica/Weathercloud Pque Tecnolgico INAOE 2019-02.csv", "wc")
-
-	// for _, v := range efm.GetRecords() {
-	// 	fmt.Println(v.(*etl.ElectricFieldRow))
-	// }
-	// db := libs.ConnectionPool()
 	libs.InitCon()
 	defer libs.CloseCon()
-	migrations.Migrate()
-	wc := services.NewWeathercloudService()
-	wc.CreateMultipleRecords("/mnt/f/DataSets/Conjuntos-originales/estacion-meteorologica/Weathercloud Pque Tecnolgico INAOE 2019-02.csv")
-	efs := services.NewElectricFieldService()
-	efs.CreateMultipleRecords("/mnt/f/DataSets/Conjuntos-originales/medidor-campo-electrico/INAOE parque-01102019.efm")
-	log := services.NewLogService()
-	log.CreateMultipleRecords("/mnt/f/DataSets/Conjuntos-originales/medidor-campo-electrico/EFMEvents.log")
+	// migrations.Migrate()
+	// wc := services.NewWeathercloudService()
+	// wc.CreateMultipleRecords("/mnt/f/DataSets/Conjuntos-originales/estacion-meteorologica/Weathercloud Pque Tecnolgico INAOE 2019-02.csv")
+	// efs := services.NewElectricFieldService()
+	// efs.CreateMultipleRecords("/mnt/f/DataSets/Conjuntos-originales/medidor-campo-electrico/INAOE parque-01102019.efm")
+	// log := services.NewLogService()
+	// log.CreateMultipleRecords("/mnt/f/DataSets/Conjuntos-originales/medidor-campo-electrico/EFMEvents.log")
 }
