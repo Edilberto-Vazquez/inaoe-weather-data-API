22ba71ee38eeb4c9206c57b5a130e45b7cca2378
Edilberto Vazquez Luna <edi_vazquez12@hotmail.com>
2021-11-24 18:59:00 -0600

    change json to form in INAOEQuerySchema

--- schemas/inaor_query_schema.go
+++ schemas/inaor_query_schema.go
@@ -3,10 +3,10 @@ package schemas
 import "time"
 
 type INAOEQuerySchema struct {
-	DatePart       string    `json:"datepart" binding:"required"`
-	FromDate       time.Time `json:"fromdate" binding:"required" time_format:"2006-01-02T15:04:05"`
-	ToDate         time.Time `json:"todate" binding:"required" time_format:"2006-01-02T15:04:05"`
-	WeatherClouds  []string  `json:"weatherclouds"`
-	ElectricFields []string  `json:"electricfields"`
-	JoinType       string    `json:"jointype"`
+	DatePart       string    `form:"datepart" binding:"required"`
+	FromDate       time.Time `form:"fromdate" binding:"required" time_format:"2006-01-02T15:04:05"`
+	ToDate         time.Time `form:"todate" binding:"required" time_format:"2006-01-02T15:04:05"`
+	WeatherClouds  []string  `form:"weatherclouds"`
+	ElectricFields []string  `form:"electricfields"`
+	JoinType       string    `form:"jointype"`
 }
